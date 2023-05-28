61ce972b36f0847b4f2671fd7ab635dee5a450f0
Veljko Bogdan <vtsveljkobogdan@gmail.com>
2023-05-28 21:01:07 +0200

    update

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>IT Berza</title>
+</head>
+<body>
+
+<header class="site-header">    <!-- Site Header-->
+    <div class="site-identity">
+        <h1><a href="#">Site Name</a></h1>
+    </div>
+    <nav class="site-navigation">
+        <ul class="nav">
+            <li><a href="#">Discover Jobs</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Sign in</a></li>
+            <li><a href="#"></a> </li>
+        </ul>
+    </nav>
+</header>
+
+</body>
+</html>
