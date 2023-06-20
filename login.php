5a0f4b4fc946ffdc220d112c64e48be32bbe3602
VeljkoBogdan <action@dr.com>
2023-06-20 20:42:31 +0200

    form updated

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ session_start();
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="#"> About</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
